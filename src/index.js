045ad4a0aec4e10bf4388be71c5449177b814146
Dmitriy Savranin <dsavranin@gmail.com>
2020-02-06 18:42:55 +0500

    feat: add some data

--- src/index.js
+++ src/index.js
@@ -3,5 +3,30 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+  for (let i = 0; i < preferences.length-1; i++) {
+	  preferences[i] = preferences[i]-1;
+  }
+  
+  let set = new Set();
+  
+  for (let i = 0; i < preferences.length-1; i++) {	  
+	  
+	  let a  = preferences[i];  
+	  let b  = preferences[i+1]; 
+	  let c  = preferences[i+2];
+	  
+	  let love_a = preferences[a];
+	  let love_b = preferences[b];
+	  let love_c = preferences[c];
+  
+	  
+	  if (love_a == b && love_b == c && love_c == a) {
+		  let abc = String(a)+String(b)+String(c);
+		  set.add(abc);			  
+	  } 
+ 
+  } 
+return set.size;
+  
 };
